8d672cdb300671f25ca2ecd4c3e1e4e2c4caaf56
Sourav Makur <souravmakur888@gmail.com>
2026-09-05 03:05:38 +0530

    practice question 2 added

--- practice_set.py
+++ practice_set.py
@@ -4,4 +4,14 @@ for i in range(7):
     fruit = input("Enter the name of fruit: ")
     fruits.append(fruit)
     print(fruits)
+    
+# 2. Write a program to store marks of 6 students in a list entered by the user.  
+marks = []
+number = 1
+for i in range(6):
+    mark = input("Enter the marks of student " + str(number) + ":" )
+    marks.append(mark)
+    number += 1
+    
+print(marks)
   
