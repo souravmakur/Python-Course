9fe16083d83208ebfa8a094518cd6fd3576afb90
Sourav Makur <souravmakur888@gmail.com>
2026-09-05 03:04:49 +0530

    practice question 1 added

--- 4. List & Tuples/tuple.py
+++ 4. List & Tuples/tuple.py
@@ -16,3 +16,13 @@ print(concatenated)
 my_tuple = (1, 2, 3)
 repeated = my_tuple * 3 #Repeat the tuple 3 times 
 print(repeated) #Concatenate the same tuple thrice
+
+print(2 in my_tuple) #Check if 2 is present in the tuple or not. It will return True or False
+print(4 in my_tuple)
+print(5 not in my_tuple) #Check if 5 is not present in the tuple or not. It will return True or False
+print(3 not in my_tuple)
+
+print(len(my_tuple)) #It will return the length of the tuple
+
+sliced = my_tuple[1:3] #Slicing the tuple. It will return a new tuple with elements from index 1 to 2 (3 is excluded)
+print(sliced) #remember that slicing a tuple will return a new tuple, it will not modify the original tuple.
---
+++ practice_set.py
@@ -0,0 +1,7 @@
+# 1. Write a program to store 7 fruits in a list entered by the user.
+fruits = []
+for i in range(7):
+    fruit = input("Enter the name of fruit: ")
+    fruits.append(fruit)
+    print(fruits)
+  
